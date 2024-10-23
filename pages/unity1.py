8e47695b2105682d968c5887047511bdadc3f196
trgyoruc <trgyoruc@gmail.com>
2024-10-23 21:04:10 +0300

    Update unity1.py

--- pages/unity1.py
+++ pages/unity1.py
@@ -18,7 +18,7 @@ def serve_build(path):
     return send_from_directory(WEBGL_DIR, path)
 
 def run_flask():
-    port = int(os.environ.get('PORT'))  # Heroku'nun atadığı portu al
+    port = int(os.environ.get('PORT', 5000))  # Heroku'nun atadığı portu al
     #port = int(os.environ.get('PORT', 5000))  # Heroku'nun atadığı portu al
     app.run(host='0.0.0.0', port=port, debug=False, use_reloader=False)
    # app.run(port=8500, debug=False, use_reloader=False)#
@@ -29,7 +29,7 @@ threading.Thread(target=run_flask).start()
 # Streamlit içinde WebGL'i görüntülemek için iframe kullanıyoruz
 st.title("WebGL Uygulaması Streamlit İçinde")
 st.markdown(f"""
-    <iframe src="http://localhost:8500" width="100%" height="600px" frameborder="0"></iframe>
+    <iframe src="https://easyphysics-fb366bb0d623.herokuapp.com/" width="100%" height="600px" frameborder="0"></iframe>
     """, unsafe_allow_html=True)
 
 # Bu projede markdown kullandik. digerinde st.components.v1
